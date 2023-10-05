33ab50ab994ba6bba53ca89e96a2dac734d4909e
Aygün Bayır <turkoaygun@gmail.com>
2023-10-05 12:51:37 +0300

    Action, action creator, reducer, store

---
+++ README.md
@@ -0,0 +1,15 @@
+# Redux Demo
+
+## Three Core Principles
+
+### First Principle
+
+The global state of your application is stored as an object inside a single store.
+
+### Second Principle
+
+The only way to change the state is to dispatch an action, an object that describes what happened
+
+### Third Principle
+
+To specify how the state tree is updated based on actions, you write pure reducers
---
+++ index.js
@@ -0,0 +1,68 @@
+const redux = require('redux');
+const createStore = redux.legacy_createStore;
+const bindActionCreators = redux.bindActionCreators;
+
+// Action
+const CAKE_ORDERED = 'CAKE_ORDERED';
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
+
+// Action Creators
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    payload: 1,
+  };
+}
+
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  };
+}
+
+// Reducer
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+// Store
+const store = createStore(reducer);
+
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log('update state ', store.getState())
+);
+
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(restockCake(3));
+
+const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch);
+actions.orderCake();
+actions.orderCake();
+actions.orderCake();
+actions.restockCake(3);
+
+unsubscribe();
